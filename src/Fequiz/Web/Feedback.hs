a70f74791d6635056f452f1130a2f48ad4146373
Betty Diegel <bdiegel@usa.net>
2009-09-13 00:49:29 +0000

    Worked on Feedback form submit handling and persisting to filesystem.

--- src/Fequiz/Web/Feedback.hs
+++ src/Fequiz/Web/Feedback.hs
@@ -5,13 +5,18 @@
 module Fequiz.Web.Feedback
    where
 
+import Control.Monad
+import Data.Maybe
 import Network.CGI
+import System.FilePath
 import System.Log
 import Text.XHtml.Strict
 
 import Fequiz.Common.Log
 import Fequiz.Common.Util
 import Fequiz.Web.Session
+import Fequiz.Web.Util
+import Paths_fequiz
 
 
 {- HTML pages and forms 
@@ -19,10 +24,11 @@ import Fequiz.Web.Session
 formFeedback :: Html
 formFeedback =  do
    let labelStyle = "display: block; width: 150px; float: left; margin: 2px 4px 6px 4px; text-align: right; vertical-align: top"
-   form << ( 
+   form ! [ method "POST" ] << ( 
       [ p << [label ! [thestyle labelStyle] << "Email: ", textfield "email"]
       , p << [label ! [thestyle labelStyle] << "Subject: ", textfield "subject"]
-      , p << [label ! [thestyle labelStyle] << "Comment: ", (textarea ! [rows "20", cols "40"]) noHtml]
+      , p << [label ! [thestyle labelStyle] << "Comment: ", 
+            (textarea ! [rows "20", cols "40", name "comment"]) noHtml]
       , p << [label ! [thestyle labelStyle] << "", submit "ActFeedback" "Submit" ! [theclass "button"] ]
       ] ) 
    
@@ -36,7 +42,27 @@ actionFeedbackPage = do
    feedbackPage <- liftIO $ page $ formFeedback
    output $ renderHtml feedbackPage
 
+
+{- Handles feedback form submit
+-}
 actionFeedbackHandler :: App CGIResult
 actionFeedbackHandler = do
    llog INFO "actionFeedbackHandler"
-   output $ renderHtml $ form << "Thank you!"
+   email <- liftM fromJust $ getInput "email"
+   subj <- liftM fromJust $ getInput "subject"
+   comment <- liftM fromJust $ getInput "comment"
+   llog DEBUG $ "email: " ++ email ++ " subj: " ++ subj ++ " comment: " ++ comment
+   liftIO $ do       
+      fname <- formattedDate "%Y%m%d%H%M%S"
+      saveFeedback fname ( "Email:" ++ email ++ "\nSubject:" ++ subj ++ "\nComment:" ++ comment )
+   output $ renderHtml $ form << ( "Thank you " ++ email )
+
+{- Saves feedback submission to file 
+-}
+saveFeedback :: String -> String -> IO ()
+saveFeedback fname fcontent = do
+   fbDir <- getDataFileName $ "feedback"
+   mkdir fbDir
+   let path = fbDir </> fname
+   logM DEBUG $ "path: " ++ path ++ " content:" ++ fcontent
+   writeFile path fcontent
