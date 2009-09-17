a4f7a0de62df21a9352fc8d998280068f7c710ab
Dino Morelli <dino@ui3.info>
2009-09-17 17:53:56 +0000

    Trying to be smarter about directory group and permissions during install
    This means the install target always requires root permissions, because of the chgrp

--- Setup.hs
+++ Setup.hs
@@ -4,7 +4,38 @@
 -- License: BSD3 (see LICENSE)
 -- Author: Ren Hoek <ren@spumco.co.dk>
 
+import Data.List
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import System.Cmd
+import System.FilePath
+import Text.Printf
 
 
-main = defaultMain
+distroSpecificHttpGroup path
+   -- Debian-based
+   | isPrefixOf "/var/www" path = "www-data"
+
+   -- Arch Linux
+   | isPrefixOf "/srv/http" path = "http"
+
+   -- Just fail if it's not one of the above cases
+   -- Add new entries for other distros as they come up
+   | otherwise = undefined
+
+
+main = defaultMainWithHooks (simpleUserHooks 
+   { postInst = customPostInst
+   } )
+   where
+      customPostInst _ _ _ localBuildInfo = do
+         let sharePath = (fromPathTemplate $ prefix
+               $ installDirTemplates localBuildInfo)
+               </> "share"
+
+         let group = distroSpecificHttpGroup sharePath
+
+         system $ printf "chgrp -R %s %s" group sharePath
+         system $ printf "chmod -R g+w %s" sharePath
+
+         return ()
