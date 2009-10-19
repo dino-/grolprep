495ebb5fba804e653288aa131f34b0e7503ca516
Betty Diegel <bdiegel@usa.net>
2009-10-19 23:30:19 +0000

    Integrated reCaptcha into Feedback form.  Added basic support for reCaptch challenge response verification.

--- src/Grolprep/Web/Feedback.hs
+++ src/Grolprep/Web/Feedback.hs
@@ -5,18 +5,51 @@
 module Grolprep.Web.Feedback
    where
 
-import Control.Monad
+import Control.Monad()
+import Control.Monad.Error
 import Data.Maybe
-import Network.CGI
+import Data.List
+import Network.CGI hiding (urlEncode)
+import Network.HTTP
+import Network.HTTP.Base
+import Network.HTTP.Headers
+import Network.Stream
+import Network.URI hiding (path)
 import System.FilePath
 import Text.Printf
-import Text.XHtml.Strict
+import Text.XHtml.Strict 
 
 import Grolprep.Common.Log
 import Grolprep.Common.Util
 import Grolprep.Web.Session
 import Grolprep.Web.Util
 import Paths_grolprep
+
+
+{- Feedback handler failures -}
+data FeedbackException a
+   = ChalRespFail String
+   | RecaptchaFail String
+   | HttpFail (Response a)
+   | NetFail ConnError
+   deriving Show
+
+
+{- It's bullshit that we have to do this -}
+instance Error (FeedbackException a) where
+   noMsg = ChalRespFail "SHOULD NEVER SEE THIS"
+
+
+{- Convenience type, not really necessary -}
+type FeedbackResult a = Either (FeedbackException a) ()
+
+
+{- Log feedback handler failures -}
+logFeedbackException :: FeedbackException a -> IO ()
+logFeedbackException (ChalRespFail s) = llog NOTICE s
+logFeedbackException (RecaptchaFail s) = llog NOTICE s
+logFeedbackException (HttpFail r) = llog ERROR $ show r
+logFeedbackException (NetFail ce) = llog ERROR $ show ce
 
 
 {- Dispatches HTML requests for /feedback URLs
@@ -39,12 +72,13 @@ dispatchFeedback = do
 formFeedback :: Html
 formFeedback =  do
    let labelStyle = "display: block; width: 150px; float: left; margin: 2px 4px 6px 4px; text-align: right; vertical-align: top"
-   form ! [ method "POST" ] << ( 
+   form ! [ method "POST" ] << (
       [ p << [label ! [thestyle labelStyle] << "Email: ", textfield "email"]
       , p << [label ! [thestyle labelStyle] << "Subject: ", textfield "subject"]
       , p << [label ! [thestyle labelStyle] << "Comment: ", 
             (textarea ! [rows "20", cols "40", name "comment"]) noHtml]
       , p << [label ! [thestyle labelStyle] << "", submit "ActFeedback" "Submit" ! [theclass "button"] ]
+      , script ! [ thetype "text/javascript", src "http://api.recaptcha.net/challenge?k=6Ldc1QgAAAAAALks41LS1WBEVKAI9rlJuxPqTOxD" ] << noHtml
       ] ) 
 
 
@@ -71,18 +105,57 @@ actionFeedbackPage = do
 actionFeedbackHandler :: App CGIResult
 actionFeedbackHandler = do
    llog INFO "actionFeedbackHandler"
+
    email <- liftM fromJust $ getInput "email"
    subj <- liftM fromJust $ getInput "subject"
    comment <- liftM fromJust $ getInput "comment"
-   --llog DEBUG $ "email: " ++ email ++ " subj: " ++ subj ++ " comment: " ++ comment
    commentorIp <- remoteAddr
+   challenge <- liftM fromJust $ getInput "recaptcha_challenge_field"
+   response <- liftM fromJust $ getInput "recaptcha_response_field"
+
    liftIO $ do       
+      e <- verifyChallengeResponse commentorIp challenge response
+      either logFeedbackException (const $ logM DEBUG "Challenge response verified") e 
       fname <- formattedDate "%Y%m%d-%H%M%S"
       saveFeedback fname $ printf
          "Submitted: %s\nIP: %s\nEmail: %s\nSubject: %s\nComment: %s\n"
          fname commentorIp email subj comment
+
    thankyouPage <- liftIO $ page [] $ pageThankYou
    output $ renderHtml thankyouPage
+
+
+{- Verifies the challenge response with the reCaptcha server
+-}
+verifyChallengeResponse :: String -> String -> String -> IO (FeedbackResult String)
+verifyChallengeResponse ip challenge response =  runErrorT $ do
+   let params = urlEncodeVars 
+                  [ ("privatekey", "6Ldc1QgAAAAAAF6txBF5_KX4pe71qdTxILZyN47F")
+                  , ("remoteip", ip)
+                  , ("challenge", challenge) 
+                  , ("response", response)
+                  ]
+   let uri = fromJust $ parseURI "http://api-verify.recaptcha.net/verify"
+   let hdrs = [ mkHeader HdrHost "api-verify.recaptcha.net"
+              , mkHeader HdrContentType "application/x-www-form-urlencoded" 
+              , mkHeader HdrContentLength (show $ length params) 
+              ]
+   let httpReq = ((mkRequest POST uri) :: Request String) { rqBody = params, rqHeaders = hdrs }
+
+   result <- liftIO $ simpleHTTP httpReq
+   resp <- either (throwError . NetFail) return result
+   respBody <- evalCode resp
+   evalBody respBody
+
+   where 
+      evalCode r@(Response (x, _, _) _ _ b)
+         | x > 2 = throwError $ HttpFail r
+         | otherwise = return b
+
+      evalBody b 
+         | isInfixOf "true" b = return ()
+         | isInfixOf "incorrect-captcha-sol" b = throwError $ RecaptchaFail b 
+         | otherwise = throwError $ ChalRespFail b
 
 
 {- Saves feedback submission to file 
