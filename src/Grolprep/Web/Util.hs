93d450f35d131bd3d8707ebe7e981bbb9675b006
Dino Morelli <dino@ui3.info>
2010-01-15 20:57:52 +0000

    Removed unused Action data type and related code from Grolprep.Web.Util
    This includes the code to figure out what button was pressed on a firm using some of its attributes. The application was changed to rely on URIs for dispatching.

--- src/Grolprep/Web/Util.hs
+++ src/Grolprep/Web/Util.hs
@@ -6,63 +6,15 @@ module Grolprep.Web.Util
    where
 
 import Control.Monad
-import Data.List
-import Data.Maybe
-import Network.CGI
 import Text.XHtml.Strict
 
 import Grolprep.Common.Util
-
-
-{- This represents a type-safe enumeration of the possible form actions
-   that are responded to by this application
-
-   Serialized values of this type are the submit button identifiers,
-   all of this achieved with read and show for safety.
--}
-data Action
-   = ActStart
-   | ActPose
-   | ActAnswer
-   | ActFeedback
-   | ActQuit
-   deriving (Read, Show)
 
 
 {- The base URL of the application
 -}
 baseUrl :: String
 baseUrl = "/grolprep/bin/fcc-grol-prep.cgi"
-
-
-{- If you add a constructor the the Action type above, you MUST add
-   it to the list of constructed datum below.
--}
-allActions :: [Action]
-allActions = [ActStart, ActPose, ActAnswer, ActFeedback, ActQuit]
-
-
-{- When a specific form submit button is pressed, and it has a name 
-   attribute, that attribute becomes a key in the request with the 
-   value attribute as the value. This can be used to determine if a 
-   button was pressed at all to generate the request and if so which 
-   one. 
-
-   But it's bad to rely on the hard-coded button's display text as it 
-   may change.
-
-   This function checks for the existance of a specific set of buttons 
-   and if it's there returns the key, not the value.
-
-   This key, in our application, is also an instance of the Action
-   type.
--}
-getButtonPressed :: (MonadCGI m) => m (Maybe Action)
-getButtonPressed = do
-   mbvs <- mapM getInput $ map show allActions
-   let mbfs = zipWith (\i a -> maybe Nothing (const $ Just a) i)
-         mbvs allActions
-   return $ foldr mplus Nothing mbfs
 
 
 createCssLinks :: [FilePath] -> IO [Html]
