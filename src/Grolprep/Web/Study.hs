21397f287f0ff14ddbce3282df1266f2ff435072
Dino Morelli <dino@ui3.info>
2017-07-02 16:41:03 +0000

    Updated link to developer website
    Closes grolprep Trello card #8

--- src/Grolprep/Web/Study.hs
+++ src/Grolprep/Web/Study.hs
@@ -136,7 +136,7 @@ formSetup = do
              thediv ! [theclass "banner"] << h2 ! [theclass "footer"] << (
                -- This is the right-side content, it floats
                thespan ! [theclass "footer-right"] << (
-                  anchor ! [theclass "footer-right", href "http://ui3.info/d/proj/grolprep"] << "developer site"
+                  anchor ! [theclass "footer-right", href "https://hub.darcs.net/dino/grolprep"] << "developer site"
                   +++
                   (thespan ! [theclass "banner-dark-text"] << primHtml " &middot; ")
                   +++
